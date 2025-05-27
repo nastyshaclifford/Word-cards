e2cf5456b7b978f751044fbeff4bc2ad4535f01b
nastyshaclifford <alieksankova2002@mail.ru>
2025-05-27 17:15:37 +0300

    обновила компоненты, удалила устаревшие файлы, добавила стили

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+function Footer() {
+    return <footer className='footer'>© 2025 My Dictionary App</footer>;
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Header.css';
+
+function Header() {
+    return <header className="header">Мой словарь</header>
+}
+
+export default Header;
